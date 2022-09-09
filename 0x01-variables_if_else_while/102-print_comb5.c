22c6fdb359ee35c0c1eef076327167fc21b618c7
Angenor92 <angenorachy8@gmail.com>
2022-09-09 16:59:18 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,17 +19,17 @@ int main(void)
 				{
 					if ((c * 10) + d > (a * 10) + b)
 					{
-						if (coma == 0)
-						{
-							putchar(',');
-							putchar(32);
-						}
-							putchar(a + '0');
-							putchar(b + '0');
-							putchar(32);
-							putchar(c + '0');
-							putchar(d + '0');
-							coma = 0;
+					if (coma == 0)
+					{
+						putchar(',');
+						putchar(32);
+					}
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(32);
+						putchar(c + '0');
+						putchar(d + '0');
+						coma = 0;
 					}
 					++d;
 				}
