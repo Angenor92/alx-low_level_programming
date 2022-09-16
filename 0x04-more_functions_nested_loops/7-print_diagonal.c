27b873fc8eec8dc53edb001d3a6afc2dd16774d0
Angenor92 <angenorachy8@gmail.com>
2022-09-16 06:54:28 -0700

    7-print_diagonal.c correction

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,17 +1,26 @@
 #include "main.h"
 /**
- * print_line - function
+ * print_diagonal - function
  *
  * @n: parameter
  *
  * Return: return value
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
+	int m = 0;
+	int i;
+
 	if (n > 0)
 	{
-		while (n--)
-			_putchar(95);
-	}
-	_putchar(10);
+		while (m < n)
+		{
+			for (i = 0; i < m; i++)
+				_putchar(32);
+			_putchar(92);
+			_putchar(10);
+			m++;
+		}
+	} else
+		_putchar(10);
 }
